f25b19fbc266d15cb17fcb9661281fee817d5302
Karolina <eszkarolina@gmail.com>
2015-02-08 14:37:43 +0100

    logowanie

--- mojblog/urls.py
+++ mojblog/urls.py
@@ -1,9 +1,19 @@
 from django.conf.urls import patterns, include, url
 from . import views
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+     url(r'^admin/', include(admin.site.urls)),
+   
+
 )
